c335e43222d8d84f5de12624107ced59c06b8b53
s3759362RMIT <s3759362@student.rmit.edu.au>
2022-08-22 17:35:08 +1000

    initialise springboot

---
+++ Backend/SEPT_Backend/src/test/java/com/example/sept_backend/SeptBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sept_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeptBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
