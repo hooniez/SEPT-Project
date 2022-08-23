dac924ec2d3bc6857b822463c3bb21fc589e6dec
Cal Lamshed <s3706239@student.rmit.edu.au>
2022-08-23 16:17:12 +1000

    adding form

--- Signup/SignupFront/signup/lib/main.dart
+++ Signup/SignupFront/signup/lib/main.dart
@@ -8,8 +8,15 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -22,122 +29,125 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'Register',
-                        style: TextStyle(
-                            color: Color.fromARGB(255, 223, 28, 93),
-                            fontSize: 15.0),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(top: 50.0),
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: "Full Name",
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15),
+            child: Form(
+              key: _formKey,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Register',
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 223, 28, 93),
+                              fontSize: 15.0),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 50.0),
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: "Full Name",
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
                           ),
                         ),
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: "Date of Birth",
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: "Date of Birth",
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
                           ),
                         ),
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: "Email",
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: "Email",
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
                           ),
                         ),
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: "Phone Number",
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: "Phone Number",
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
                           ),
                         ),
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: "Date of Birth",
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: "Date of Birth",
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
 
-                // Second Column in row
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      TextFormField(
-                        maxLines: 6,
-                        decoration: InputDecoration(
-                          labelText: "Medical History",
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15),
+                  // Second Column in row
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextFormField(
+                          maxLines: 6,
+                          decoration: InputDecoration(
+                            labelText: "Medical History",
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
                           ),
                         ),
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: "Password",
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: "Password",
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
                           ),
                         ),
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: "Confirm Password",
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: "Confirm Password",
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        width: 300.0, // <-- match_parent
-                        height: 50.0, // <-- match-parent
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.red, // background
-                            onPrimary: Colors.white, // foreground
+                        SizedBox(
+                          width: 300.0, // <-- match_parent
+                          height: 50.0, // <-- match-parent
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.red, // background
+                              onPrimary: Colors.white, // foreground
+                            ),
+                            child: const Text('Register'),
+                            onPressed: () {
+                              print('Hello');
+                            },
                           ),
-                          child: const Text('Register'),
-                          onPressed: () {
-                            print('Hello');
-                          },
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
             padding: const EdgeInsets.all(15),
             color: Colors.blue,
