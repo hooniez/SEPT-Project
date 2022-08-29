4b6841811ca9ad88bc781d9297a6b50f1122580f
Cal Lamshed <s3706239@student.rmit.edu.au>
2022-08-29 18:16:16 +1000

    Creating UI

--- Frontend/frontend/lib/frontPage.dart
+++ Frontend/frontend/lib/frontPage.dart
@@ -4,14 +4,14 @@ import 'dart:html';
 
 import 'package:flutter/material.dart';
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+class FrontPage extends StatefulWidget {
+  const FrontPage({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<FrontPage> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<FrontPage> {
   final _formKey = GlobalKey<FormState>();
 
   @override
@@ -21,67 +21,55 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: const Color.fromARGB(255, 223, 28, 93),
-          title: const Text("Neighbourhood Doctors Login"),
+          title: const Text("Welcome to Neighbourhood Doctors"),
         ),
         body: Center(
           child: Container(
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Login',
-                    style: TextStyle(color: Colors.white, fontSize: 28.0),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(top: 40.0),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: "Email",
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Welcome',
+                  style: TextStyle(color: Colors.white, fontSize: 28.0),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 40.0),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: SizedBox(
+                    width: 350.0, // <-- match_parent
+                    height: 75.0, // <-- match-parent
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.lightBlue, // background
+                        onPrimary: Colors.white, // foreground
                       ),
+                      child: const Text('Login'),
+                      onPressed: () {
+                        print('Hello');
+                      },
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: "Password",
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: SizedBox(
+                    width: 350.0, // <-- match_parent
+                    height: 75.0, // <-- match-parent
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.lightGreen, // background
+                        onPrimary: Colors.black, // foreground
                       ),
+                      child: const Text('Register'),
+                      onPressed: () {
+                        print('Hello');
+                      },
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: SizedBox(
-                      width: 300.0, // <-- match_parent
-                      height: 50.0, // <-- match-parent
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.red, // background
-                          onPrimary: Colors.white, // foreground
-                        ),
-                        child: const Text('Login'),
-                        onPressed: () {
-                          print('Hello');
-                        },
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
             padding: const EdgeInsets.all(15),
             color: Color.fromARGB(255, 113, 113, 113),
