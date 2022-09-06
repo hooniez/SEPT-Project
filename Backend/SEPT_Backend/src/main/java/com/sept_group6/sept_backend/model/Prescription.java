7ffedeac83423c3d002f91b045548d651bd63e5b
MCFoord <max.foord@hotmail.com>
2022-09-06 21:46:41 +1000

    base model getter setter added

--- Backend/SEPT_Backend/src/main/java/com/sept_group6/sept_backend/model/Prescription.java
+++ Backend/SEPT_Backend/src/main/java/com/sept_group6/sept_backend/model/Prescription.java
@@ -1,5 +1,41 @@
 package com.sept_group6.sept_backend.model;
 
 public class Prescription {
+
+    private Integer id;
+    private Integer patientid;
+    private String prescriptiondetails;
+    private String prescriptioninstructions;
+
+    public Integer getId() {
+        return id;        
+    }
     
+    public Integer getPatientid() {
+        return patientid;        
+    }
+    
+    public String getPrescriptiondetails() {
+        return prescriptiondetails;        
+    }
+    
+    public String getPrescriptioninstructions() {
+        return prescriptioninstructions;        
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public void setPatientid(Integer patientid) {
+        this.patientid = patientid;
+    }
+    
+    public void setPrescriptiondetails(String prescriptiondetails) {
+        this.prescriptiondetails = prescriptiondetails;
+    }
+    
+    public void setPrescriptioninstructions(String prescriptioninstructions) {
+        this.prescriptioninstructions = prescriptioninstructions;
+    }
 }
