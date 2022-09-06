cd7c16d8d3b87adf57cdcc3189cf234b27b4f21b
s3759362RMIT <s3759362@student.rmit.edu.au>
2022-09-07 02:04:33 +1000

    integrate database support to springboot

--- Backend/SEPT_Backend/src/main/java/com/sept_group6/sept_backend/controllers/SignupController.java
+++ Backend/SEPT_Backend/src/main/java/com/sept_group6/sept_backend/controllers/SignupController.java
@@ -16,50 +16,49 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import com.sept_group6.sept_backend.dao.UserDAO;
-import com.sept_group6.sept_backend.model.Users;
-import com.sept_group6.sept_backend.model.User;
+import com.sept_group6.sept_backend.dao.PatientRepository;
+import com.sept_group6.sept_backend.model.Patient;
 
 /*TODO: turn this resfful */
 @RestController
 @RequestMapping(path = "/signup")
 public class SignupController {
 
-    private static final Logger logger = LogManager.getLogger("Backend");
+        private static final Logger logger = LogManager.getLogger("Backend");
 
-    @Autowired
-    private UserDAO userDao;
+        @Autowired
+        private PatientRepository patientRepository;
 
-    // /* not need for signup at the moment */
-    // @GetMapping(path = "", produces = "application/json")
-    // public Users getUsers() {
-    // logger.info("signup - get");
-    // return new Users();
-    // }
+        // /* not need for signup at the moment */
+        // @GetMapping(path = "", produces = "application/json")
+        // public Users getUsers() {
+        // logger.info("signup - get");
+        // return new Users();
+        // }
 
-    @PutMapping(path = "", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<Object> addUser(
-            @RequestHeader(name = "X-COM-PERSIST", required = false) String headerPersist,
-            @RequestHeader(name = "X-COM-LOCATION", required = false, defaultValue = "AUS") String headerLocation,
-            @RequestBody User user)
-            throws Exception {
-        logger.info("signup - put");
-        // Generate resource id
-        Integer id = userDao.getNumOfUsers() + 1;
-        logger.info(id);
-        user.setId(id);
+        @PutMapping(path = "", consumes = "application/json", produces = "application/json")
+        public ResponseEntity<Object> addUser(
+                        @RequestHeader(name = "X-COM-PERSIST", required = false) String headerPersist,
+                        @RequestHeader(name = "X-COM-LOCATION", required = false, defaultValue = "AUS") String headerLocation,
+                        @RequestBody Patient newPatient)
+                        throws Exception {
+                logger.info("signup - put");
+                // Generate resource id
+                Integer id = userDao.getNumOfUsers() + 1;
+                logger.info(id);
+                user.setId(id);
 
-        // add resource
-        userDao.addUser(user);
-        logger.info(userDao.getNumOfUsers());
+                // add resource
+                userDao.addUser(user);
+                logger.info(userDao.getNumOfUsers());
 
-        // create resource location
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(user.getId())
-                .toUri();
+                // create resource location
+                URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                                .path("/{id}")
+                                .buildAndExpand(user.getId())
+                                .toUri();
 
-        // // Send location in response
-        return ResponseEntity.created(location).build();
-    }
+                // // Send location in response
+                return ResponseEntity.created(location).build();
+        }
 }
