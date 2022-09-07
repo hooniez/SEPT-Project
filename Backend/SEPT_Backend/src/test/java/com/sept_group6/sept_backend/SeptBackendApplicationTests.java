ab2c81a1963ee5ace68fb9a8bddb956e6a4cea18
s3759362RMIT <s3759362@student.rmit.edu.au>
2022-09-07 16:21:38 +1000

    update test folder structure

---
+++ Backend/SEPT_Backend/src/test/java/com/sept_group6/sept_backend/SeptBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sept_group6.sept_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeptBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
