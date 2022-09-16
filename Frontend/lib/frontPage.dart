dfc60aebbf13ff2401ca2a579baba8bb8689e4e7
hooniez <s3774430@student.rmit.edu.au>
2022-09-16 12:58:40 +1000

    Complete the feature

--- Frontend/lib/frontPage.dart
+++ Frontend/lib/frontPage.dart
@@ -22,16 +22,16 @@ class FrontPage extends StatelessWidget {
       home: Scaffold(
           appBar: AppBar(
             backgroundColor: const Color.fromARGB(255, 223, 28, 93),
-            title: Center(child: const Text("Neighbourhood Doctors")),
+            title: const Center(child: Text("Neighbourhood Doctors")),
           ),
           body: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 user.value.isEmpty
-                    ? Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: const Text(
+                    ? const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
                           'Welcome',
                           style: TextStyle(color: Colors.black, fontSize: 24.0),
                         ),
@@ -40,14 +40,14 @@ class FrontPage extends StatelessWidget {
                         padding: const EdgeInsets.all(8.0),
                         child: Text(
                           'Welcome ${user.value['firstname']}!',
-                          style: TextStyle(color: Colors.black, fontSize: 24.0),
+                          style: const TextStyle(color: Colors.black, fontSize: 24.0),
                         ),
                       ),
                 user.value.isEmpty
                     ? Column(
                         children: [
                           Padding(
-                            padding: EdgeInsets.all(8.0),
+                            padding: const EdgeInsets.all(8.0),
                             child: SizedBox(
                               width: 200,
                               height: 50,
@@ -58,7 +58,7 @@ class FrontPage extends StatelessWidget {
                                     return Login(setUser: setUser);
                                   }));
                                 },
-                                child: Text(
+                                child: const Text(
                                   "Login",
                                   style: TextStyle(fontSize: 18.0),
                                 ),
@@ -66,7 +66,7 @@ class FrontPage extends StatelessWidget {
                             ),
                           ),
                           Padding(
-                            padding: EdgeInsets.all(8.0),
+                            padding: const EdgeInsets.all(8.0),
                             child: SizedBox(
                               width: 200,
                               height: 50,
@@ -77,7 +77,7 @@ class FrontPage extends StatelessWidget {
                                     return SignupPage(setUser: setUser);
                                   }));
                                 },
-                                child: Text("Register",
+                                child: const Text("Register",
                                     style: TextStyle(fontSize: 18.0)),
                               ),
                             ),
@@ -87,7 +87,7 @@ class FrontPage extends StatelessWidget {
                     : Column(
                         children: [
                           Padding(
-                            padding: EdgeInsets.all(8.0),
+                            padding: const EdgeInsets.all(8.0),
                             child: SizedBox(
                               width: 200,
                               height: 50,
@@ -98,15 +98,33 @@ class FrontPage extends StatelessWidget {
                                     return Login(setUser: setUser);
                                   }));
                                 },
-                                child: Text(
+                                child: const Text(
                                   "Profile",
                                   style: TextStyle(fontSize: 18.0),
                                 ),
                               ),
                             ),
                           ),
+                          if (user.value['usertype'] == "patient") Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: SizedBox(
+                              width: 200,
+                              height: 50,
+                              child: OutlinedButton(
+                                onPressed: () {
+                                  Navigator.push(context,
+                                      MaterialPageRoute(builder: (context) {
+                                        return SignupPage(setUser: setUser);
+                                      }));
+                                },
+                                child: const Text("Doctors",
+                                    style: TextStyle(fontSize: 18.0)),
+                              ),
+                            ),
+                          ),
+
                           Padding(
-                            padding: EdgeInsets.all(8.0),
+                            padding: const EdgeInsets.all(8.0),
                             child: SizedBox(
                               width: 200,
                               height: 50,
@@ -117,13 +135,13 @@ class FrontPage extends StatelessWidget {
                                     return SignupPage(setUser: setUser);
                                   }));
                                 },
-                                child: Text("Appointments",
+                                child: const Text("Appointments",
                                     style: TextStyle(fontSize: 18.0)),
                               ),
                             ),
                           ),
                           Padding(
-                            padding: EdgeInsets.all(8.0),
+                            padding: const EdgeInsets.all(8.0),
                             child: SizedBox(
                               width: 200,
                               height: 50,
@@ -134,13 +152,13 @@ class FrontPage extends StatelessWidget {
                                     return SignupPage(setUser: setUser);
                                   }));
                                 },
-                                child: Text("Chat",
+                                child: const Text("Chat",
                                     style: TextStyle(fontSize: 18.0)),
                               ),
                             ),
                           ),
                           Padding(
-                            padding: EdgeInsets.all(8.0),
+                            padding: const EdgeInsets.all(8.0),
                             child: SizedBox(
                               width: 200,
                               height: 50,
@@ -148,7 +166,7 @@ class FrontPage extends StatelessWidget {
                                 onPressed: () {
                                   logoutUser();
                                 },
-                                child: Text("Logout",
+                                child: const Text("Logout",
                                     style: TextStyle(
                                         color: Colors.red, fontSize: 18.0)),
                               ),
@@ -159,65 +177,6 @@ class FrontPage extends StatelessWidget {
               ],
             ),
           )),
-      // body: Center(
-      //   child: Container(
-      //     child: Column(
-      //       mainAxisAlignment: MainAxisAlignment.center,
-      //       children: [
-      //         const Text(
-      //           'Welcome',
-      //           style: TextStyle(color: Colors.black, fontSize: 28.0),
-      //         ),
-      //         const Padding(
-      //           padding: EdgeInsets.only(top: 40.0),
-      //         ),
-      //         Padding(
-      //           padding: const EdgeInsets.all(16.0),
-      //           child: SizedBox(
-      //             width: 350.0, // <-- match_parent
-      //             height: 75.0, // <-- match-parent
-      //             child: ElevatedButton(
-      //               style: ElevatedButton.styleFrom(
-      //                 primary: Colors.lightBlue, // background
-      //                 onPrimary: Colors.white, // foreground
-      //               ),
-      //               child: const Text('Login'),
-      //               onPressed: () {
-      //                 Navigator.push(context, MaterialPageRoute(builder: (context) {
-      //                   return Login(setUser: setUser);
-      //                 }));
-      //               },
-      //             ),
-      //           ),
-      //         ),
-      //         Padding(
-      //           padding: const EdgeInsets.all(16.0),
-      //           child: SizedBox(
-      //             width: 350.0, // <-- match_parent
-      //             height: 75.0, // <-- match-parent
-      //             child: ElevatedButton(
-      //               style: ElevatedButton.styleFrom(
-      //                 primary: Colors.lightGreen, // background
-      //                 onPrimary: Colors.black, // foreground
-      //               ),
-      //               child: const Text('Register'),
-      //               onPressed: () {
-      //                 Navigator.push(context, MaterialPageRoute(builder: (context) {
-      //                   return SignupPage(setUser: setUser);
-      //                 }));
-      //               },
-      //             ),
-      //           ),
-      //         ),
-      //       ],
-      //     ),
-      //     padding: const EdgeInsets.all(15),
-      //     color: Color.fromARGB(255, 113, 113, 113),
-      //     width: 500,
-      //     height: 500,
-      //   ),
-      // ),
-      // ),
     );
   }
 }
