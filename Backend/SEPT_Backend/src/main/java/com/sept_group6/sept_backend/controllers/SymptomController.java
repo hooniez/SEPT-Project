348b6898d0fa87a852433b088520eff7f832cdba
Lachlan <s3896004@student.rmit.edu.au>
2022-09-17 15:53:48 +1000

    connections between database added

---
+++ Backend/SEPT_Backend/src/main/java/com/sept_group6/sept_backend/controllers/SymptomController.java
@@ -0,0 +1,33 @@
+package com.sept_group6.sept_backend.controllers;
+
+import com.sept_group6.sept_backend.dao.SymptomRepository;
+import com.sept_group6.sept_backend.model.Symptom;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping()
+public class SymptomController {
+    private static final Logger logger = LogManager.getLogger("Backend");
+    @Autowired
+    private SymptomRepository symptomRepository;
+
+    @PutMapping(path = "/addsymptom", consumes="application/json", produces =
+            "application/json")
+    public ResponseEntity<?> addSymptom(@RequestBody Symptom newSymptom)
+            throws Exception {
+        logger.info(newSymptom);
+
+        // add resource
+        Symptom symptom = symptomRepository.save(newSymptom);
+
+        return ResponseEntity.accepted().body(symptom);
+
+    }
+}
+
--- Frontend/lib/symptomsPageCurrentSymptoms.dart
+++ Frontend/lib/symptomsPageCurrentSymptoms.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/scrollercontroller.dart';
+import 'symptomsPageAddSymptoms.dart';
 import 'package:http/http.dart';
 import 'package:intl/intl.dart';
 
@@ -20,14 +21,7 @@ class SymptomsPageCurrentSymptoms extends StatefulWidget {
 class _SymptomsPageCurrentSymptomsState
     extends State<SymptomsPageCurrentSymptoms> {
   final _formKey = GlobalKey<FormState>();
-  TextEditingController firstNameController = TextEditingController();
-  TextEditingController lastNameController = TextEditingController();
-  TextEditingController dOBController = TextEditingController();
-  TextEditingController emailController = TextEditingController();
-  TextEditingController mobileNumController = TextEditingController();
-  TextEditingController medHisController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-  TextEditingController passwordConfirmController = TextEditingController();
+  TextEditingController symptomDescriptionController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -45,119 +39,92 @@ class _SymptomsPageCurrentSymptomsState
                     child: Icon(
                       Icons.arrow_back_ios,
                       color: Colors.white,
-                    ))),
+                    )),
+                actions: <Widget>[
+                  Padding(
+                      padding: EdgeInsets.only(right: 20.0),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/frontPage');
+                        },
+                        child: Icon(
+                          Icons.home,
+                          size: 26.0,
+                        ),
+                      )),
+                ]),
             body: SingleChildScrollView(
-              controller: AdjustableScrollController(100),
-              child: Container(
-                child: Form(
-                  key: _formKey,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.all(30.0),
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(24.0),
-                                  child: SizedBox(
-                                    width: 300.0, // <-- match_parent
-                                    height: 50.0, // <-- match-parent
-                                    child: ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                        primary: Color.fromARGB(
-                                            255, 221, 150, 17), // background
-                                        onPrimary: Colors.white, // foreground
+                controller: AdjustableScrollController(100),
+                child: Container(
+                  child: Form(
+                    key: _formKey,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(30.0),
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(24.0),
+                                    child: SizedBox(
+                                      width: 300.0, // <-- match_parent
+                                      height: 50.0, // <-- match-parent
+                                      child: ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                          primary: Color.fromARGB(
+                                              255, 221, 150, 17), // background
+                                          onPrimary: Colors.white, // foreground
+                                        ),
+                                        child: const Text(
+                                          'Current Symptoms',
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                        onPressed: () async {
+                                          Navigator.push(context,
+                                              MaterialPageRoute(
+                                                  builder: (context) {
+                                            return SymptomsPageCurrentSymptoms(
+                                                getUser: widget.getUser);
+                                          }));
+                                        },
                                       ),
-                                      child: const Text(
-                                        'Current Symptoms',
-                                        style: TextStyle(color: Colors.white),
-                                      ),
-                                      onPressed: () async {},
                                     ),
                                   ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(24.0),
-                                  child: SizedBox(
-                                    width: 300.0, // <-- match_parent
-                                    height: 50.0, // <-- match-parent
-                                    child: ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                        primary: Color.fromARGB(
-                                            255, 144, 119, 151), // background
-                                        onPrimary: Colors.white, // foreground
+                                  Padding(
+                                    padding: const EdgeInsets.all(24.0),
+                                    child: SizedBox(
+                                      width: 300.0, // <-- match_parent
+                                      height: 50.0, // <-- match-parent
+                                      child: ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                          primary: Color.fromARGB(
+                                              255, 144, 119, 151), // background
+                                          onPrimary: Colors.white, // foreground
+                                        ),
+                                        child: const Text(
+                                          'Add Symptom',
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                        onPressed: () async {
+                                          Navigator.push(context,
+                                              MaterialPageRoute(
+                                                  builder: (context) {
+                                            return SymptomsPageAddSymptoms(
+                                                getUser: widget.getUser);
+                                          }));
+                                        },
                                       ),
-                                      child: const Text(
-                                        'Add Symptoms',
-                                        style: TextStyle(color: Colors.white),
-                                      ),
-                                      onPressed: () async {
-                                        print(
-                                            widget.getUser.value['firstname']);
-                                      },
                                     ),
                                   ),
-                                ),
-                              ]),
+                                ]),
+                          ),
                         ),
-                      ),
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.all(30.0),
-                          child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(24.0),
-                                  child: SizedBox(
-                                    width: 300.0, // <-- match_parent
-                                    height: 50.0, // <-- match-parent
-                                    child: ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                        primary: Color.fromARGB(
-                                            255, 221, 150, 17), // background
-                                        onPrimary: Colors.white, // foreground
-                                      ),
-                                      child: const Text(
-                                        'Current Symptoms',
-                                        style: TextStyle(color: Colors.white),
-                                      ),
-                                      onPressed: () async {},
-                                    ),
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(24.0),
-                                  child: SizedBox(
-                                    width: 300.0, // <-- match_parent
-                                    height: 50.0, // <-- match-parent
-                                    child: ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                        primary: Color.fromARGB(
-                                            255, 144, 119, 151), // background
-                                        onPrimary: Colors.white, // foreground
-                                      ),
-                                      child: const Text(
-                                        'Add Symptoms',
-                                        style: TextStyle(color: Colors.white),
-                                      ),
-                                      onPressed: () async {
-                                        print(
-                                            widget.getUser.value['firstname']);
-                                      },
-                                    ),
-                                  ),
-                                ),
-                              ]),
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ),
-            )));
+                ))));
   }
 }
