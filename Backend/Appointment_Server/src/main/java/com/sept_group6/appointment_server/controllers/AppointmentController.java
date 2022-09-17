d889ffa866ee674aa4dd76cddb665a9b4de3f9b0
s3759362RMIT <s3759362@student.rmit.edu.au>
2022-09-17 12:52:28 +1000

    bug fix

--- Backend/Appointment_Server/src/main/java/com/sept_group6/appointment_server/controllers/AppointmentController.java
+++ Backend/Appointment_Server/src/main/java/com/sept_group6/appointment_server/controllers/AppointmentController.java
@@ -10,25 +10,28 @@ import com.sept_group6.appointment_server.dao.*;
 import com.sept_group6.appointment_server.model.*;
 
 import java.util.Optional;
+import java.util.List;
 
 @RestController
-@RequestMapping(path = "/Appointment")
+@RequestMapping(path = "/appointment")
 public class AppointmentController {
     private static final Logger logger = LogManager.getLogger("Backend");
     @Autowired
     private PatientRepository patientRepository;
+    @Autowired
     private DoctorRepository doctorRepository;
+    @Autowired
     private AppointmentRepository appointmentRepository;
 
     @GetMapping("/")
     public ResponseEntity<?> loginUser(@RequestParam("email") String email, @RequestParam("usertype") String usertype) {
-        Optional<Appointment> appointments;
+        Optional<List<Appointment>> appointments;
         if (usertype.equals("patient")) {
             appointments = appointmentRepository.findByPatientEmail(email);
         } else {
             appointments = appointmentRepository.findByDoctorEmail(email);
         }
-
+        logger.info(appointments);
         if (appointments.isPresent()) {
 
             return ResponseEntity.accepted().body(appointments.get());
