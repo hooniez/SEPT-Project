2108f631a7d6290ecce84a2a7f91a2fd2dc5a14d
MCFoord <max.foord@hotmail.com>
2022-09-17 17:20:26 +1000

    updated server on message methods

--- chat_service/src/main/java/com/ChatService/config/SocketConfig.java
+++ chat_service/src/main/java/com/ChatService/config/SocketConfig.java
@@ -18,6 +18,6 @@ public class SocketConfig implements WebSocketMessageBrokerConfigurer {
   
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-      registry.addEndpoint("/gs-guide-websocket").withSockJS();
+      registry.addEndpoint("/ws").withSockJS();
     }
 }
--- chat_service/src/main/java/com/ChatService/controller/GreetingController.java
+++ chat_service/src/main/java/com/ChatService/controller/GreetingController.java
@@ -3,8 +3,6 @@ package com.ChatService.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.util.HtmlUtils;
 
 import com.ChatService.model.Message;
 
@@ -13,8 +11,10 @@ public class GreetingController {
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Message greeting(Message message) throws Exception {
-      Thread.sleep(1000); // simulated delay
-      return new Message("us", "hello!");
+        System.out.println("got a message!");
+        System.out.println(message.getText());
+        Thread.sleep(1000); // simulated delay
+        return new Message("us", "hello!");
     }
   
 }
