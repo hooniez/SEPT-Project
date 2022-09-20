ac4954c7607ff772d6eb63e3a1f88f2ed0e15d31
Lachlan <s3896004@student.rmit.edu.au>
2022-09-20 16:43:36 +1000

    add imports

--- Backend/symptom/src/main/java/com/sept_group6/symptom/SymptomApplication.java
+++ Backend/symptom/src/main/java/com/sept_group6/symptom/SymptomApplication.java
@@ -2,6 +2,12 @@ package com.sept_group6.symptom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import java.util.Collections;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SymptomApplication {
