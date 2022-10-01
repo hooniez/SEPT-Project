4e79a7dfe467cdfc9bdce4600fce9b597684a134
s3759362RMIT <s3759362@student.rmit.edu.au>
2022-10-02 00:08:54 +1000

    create generic User and UserRepository classes

--- Backend/SEPT_Backend/src/main/java/com/sept_group6/sept_backend/model/Patient.java
+++ Backend/SEPT_Backend/src/main/java/com/sept_group6/sept_backend/model/Patient.java
@@ -1,45 +1,31 @@
 package com.sept_group6.sept_backend.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 
 @Entity
 @Getter
-@Builder
+
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Patient {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long uid;
-    @Email(message = "Email must be valid")
-    @NotBlank(message = "Email is required")
-    // uniqueness when adding new row needs to be checked in contronller
-    @Column(unique = true)
-    private String email;
-    private String firstname;
-    private String lastname;
+@ToString(callSuper = true)
+public class Patient extends User {
 
-    private String dob;
-    @NotBlank(message = "Password is required")
-    private String password;
-    private String mobilenumber;
     private String medicalhistory;
 
-    public String toString() {
-        return " email= " + email + " firstName= " + firstname + " lastName= " +
-                lastname +
-                " dob= " + dob + " password= " + password + " mobileNumber= " + mobilenumber
-                + " medicalHistory= "
-                + medicalhistory;
+    public Patient(long uid, String email, String firstname, String lastname, String dob, String password,
+            String mobilenumber, String medicalhistory) {
+        super(uid, email, firstname, lastname, dob, password, mobilenumber);
+        this.medicalhistory = medicalhistory;
     }
 
 }
