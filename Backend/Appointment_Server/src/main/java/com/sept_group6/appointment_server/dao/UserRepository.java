82a67ee6d2bb217092e35611818a1ea6aed91f42
s3759362RMIT <s3759362@student.rmit.edu.au>
2022-10-02 13:10:54 +1100

    apply JWT to appointment server

---
+++ Backend/Appointment_Server/src/main/java/com/sept_group6/appointment_server/dao/UserRepository.java
@@ -0,0 +1,18 @@
+package com.sept_group6.appointment_server.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+import com.sept_group6.appointment_server.model.User;
+
+import java.util.Optional;
+
+@NoRepositoryBean
+public interface UserRepository<T extends User> extends CrudRepository<T, Long> {
+    Optional<T> findByEmailAndPassword(String email, String password);
+
+    T findByEmail(String email);
+
+    T getById(Long id);
+
+    boolean existsByEmail(String Email);
+}
---
+++ Backend/Appointment_Server/src/main/java/com/sept_group6/appointment_server/security/JwtUserDetailsService.java
@@ -0,0 +1,60 @@
+package com.sept_group6.appointment_server.security;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import com.sept_group6.appointment_server.dao.PatientRepository;
+import com.sept_group6.appointment_server.dao.DoctorRepository;
+import com.sept_group6.appointment_server.model.User;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+    private static final Logger logger = LogManager.getLogger("Backend");
+
+    @Autowired
+    private PatientRepository patientRepository;
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        logger.error("wrong userDetailService");
+        new UsernameNotFoundException("User not found");
+        return null;
+    }
+
+    @Transactional
+    public JwtUserDetails loadUserByEmailAndUsertype(String email, String userType) throws UsernameNotFoundException {
+
+        User user;
+        if (userType.equals("patient")) {
+            user = patientRepository.findByEmail(email);
+        } else {
+            user = doctorRepository.findByEmail(email);
+        }
+        if (user == null)
+            new UsernameNotFoundException("User not found");
+        return new JwtUserDetails(user.getUid(), user.getEmail(), user.getPassword(), userType);
+    }
+
+    @Transactional
+    public JwtUserDetails loadUserByIdAndUserType(Long id, String userType) {
+        User user;
+        if (userType.equals("patient")) {
+            user = patientRepository.getById(id);
+        } else {
+            user = patientRepository.getById(id);
+        }
+
+        if (user == null)
+            new UsernameNotFoundException("User not found");
+        return new JwtUserDetails(user.getUid(), user.getEmail(), user.getPassword(), userType);
+    }
+}
