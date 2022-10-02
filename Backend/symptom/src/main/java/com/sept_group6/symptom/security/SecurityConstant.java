a35033bb788d67e035f1119bb522c8e40fc026be
s3759362RMIT <s3759362@student.rmit.edu.au>
2022-10-02 18:08:04 +1100

    updare sympton server to use JWT

---
+++ Backend/symptom/src/main/java/com/sept_group6/symptom/security/SecurityConstant.java
@@ -0,0 +1,12 @@
+package com.sept_group6.symptom.security;
+
+public class SecurityConstant {
+    // public static final String SIGNIN_URLS = "/**/signin";
+    // public static final String SIGNUP_URLS = "/**/signup";
+    // public static final String H2_URL = "/h2-console/**";
+    // secret key
+    public static final String SECRET = "SecretKeyToGenJWTs";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final long EXPIRATION_TIME = 300_000; // 5 minutes
+}
