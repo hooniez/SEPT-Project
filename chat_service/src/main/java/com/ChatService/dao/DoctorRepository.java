ebe3a248873db0f1fea25a04dc606cb06cbd1b2a
S3759362rmit <82450159+S3759362rmit@users.noreply.github.com>
2022-10-12 23:36:54 +1100

    Revert "Livechat"

--- chat_service/src/main/java/com/ChatService/dao/DoctorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ChatService.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import com.ChatService.model.Doctor;
-
-public interface DoctorRepository extends CrudRepository<Doctor, Long> {
-
-}
