185c217b9696eeff9eb11ab2e85b9f2dc80165d5
Lachlan <s3896004@student.rmit.edu.au>
2022-10-12 12:40:28 +1100

    appointment server breaking

--- Backend/Profile/src/test/java/com/sept_group6/profile/ProfileApplicationTests.java
+++ Backend/Profile/src/test/java/com/sept_group6/profile/ProfileApplicationTests.java
@@ -3,7 +3,7 @@ package com.sept_group6.profile;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ProfileApplicationTests.class)
 class ProfileApplicationTests {
 
     @Test
