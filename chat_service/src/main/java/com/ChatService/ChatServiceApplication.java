27163891565951759ff28ac90ed435a6a6a8b3b5
MCFoord <max.foord@hotmail.com>
2022-10-13 13:28:06 +1100

    fix

--- chat_service/src/main/java/com/ChatService/ChatServiceApplication.java
+++ chat_service/src/main/java/com/ChatService/ChatServiceApplication.java
@@ -2,8 +2,9 @@ package com.ChatService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ChatServiceApplication {
 
 	public static void main(String[] args) {
