55ca3be06d7a7098888d63fdca4e6e5425265be9
MCFoord <max.foord@hotmail.com>
2022-10-18 02:10:48 +1100

    added repository tags

--- chat_service/src/main/java/com/ChatService/dao/PatientRepository.java
+++ chat_service/src/main/java/com/ChatService/dao/PatientRepository.java
@@ -2,7 +2,9 @@ package com.ChatService.dao;
 
 import org.springframework.data.repository.CrudRepository;
 import com.ChatService.model.Patient;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PatientRepository extends CrudRepository<Patient, Long> {
 
 }
