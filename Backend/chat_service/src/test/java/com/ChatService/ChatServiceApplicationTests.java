9fd46ef999ad68c08b8b453c307c1ee9d3a5df76
hooniez <s3774430@student.rmit.edu.au>
2022-10-18 23:00:36 +1100

    Fix

--- Backend/chat_service/src/test/java/com/ChatService/ChatServiceApplicationTests.java
+++ Backend/chat_service/src/test/java/com/ChatService/ChatServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.ChatService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ChatServiceApplicationTests.class)
 class ChatServiceApplicationTests {
 
 	@Test
